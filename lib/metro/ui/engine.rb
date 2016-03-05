1943dca0f90c8741799257f7bcec6c65e2773b20
sven199109 <sven199109@gmail.com>
2016-03-05 17:36:19 +0800

    add precompile

--- lib/metro/ui/engine.rb
+++ lib/metro/ui/engine.rb
@@ -3,8 +3,12 @@ module Metro
     class Engine < ::Rails::Engine 
       initializer 'metro-ui.assets.precompile' do |app|
         %w(stylesheets javascripts fonts images).each do |sub|
-          # app.config.assets.paths << root.join('assets', sub).to_s
+          app.config.assets.paths << root.join('app/assets', sub).to_s
         end
+
+        app.config.assets.precompile << %r(metro\.(?:eot|svg|ttf|woff)$)
+        app.config.assets.precompile << %w(selection.json metro.min.js)
+        app.config.assets.precompile << %w(*\.css)
       end
     end
   end
